fe5647bf34105fb171d4c8d9f59d5a4210425e88
lucasvbento <167943532+lucasvbento@users.noreply.github.com>
2025-05-06 16:02:55 -0300

    trocando projeto

--- API_CEOS/alunos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Aluno
-
-@admin.register(Aluno)
-class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'matricula', 'curso', 'email', 'ano_entrada', 'criacao', 'atualizacao', 'ativo')
-    search_fields = ('matricula',)
--- API_CEOS/alunos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AlunosAPIView, AlunoAPIView
-
-urlpatterns = [
-    path('alunos/', AlunosAPIView.as_view(), name='alunos'),
-    path('alunos/<int:pk>', AlunoAPIView.as_view(), name='aluno')
-    
-
-]
